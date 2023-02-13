0cdf1f6a3e26e68258791fd0252ca8b350c0b8f3
DavidGLeal <adguarderas@espe.edu.ec>
2023-02-13 04:00:05 -0500

    Correc login

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -12,25 +12,28 @@ function Popup(props) {
                 {props.children}
                 <div className='mainPopup'>
                     <div className='contenidoPopup grid'>
-                        <button class="cuentas" id="google">Iniciar sesión con Google</button><br/>
-                        <button class="cuentas" id="facebook">Iniciar sesión con Facebook</button><br/>
                         <h1>Iniciar Sesión</h1>
+                        
                         <div className='formulario'>
-                            <label for="correo">Correo: </label>
+                            <label for="correo">Correo</label>
                             <input type="text" class="inputFormulario login" id="nombre" name="nombre" onChange={(e) => {
                                 setCorreo(e.target.value)
                             }} />
-                            <label for="contraseña">Contraseña:</label>
+                            <label for="contraseña">Contraseña</label>
                             <input type="password" class="inputFormulario login" id="contraseña" name="contraseña" onChange={(e) => {
                                 setContrasenia(e.target.value)
                             }} />
                             <br/>
                             <button>Iniciar Sesión</button>
                         </div>
+                        <hr/>
+                        <button class="cuentas" id="google">Iniciar sesión con Google</button><br/>
+                        <button class="cuentas" id="facebook">Iniciar sesión con Facebook</button><br/>
                         <p id="regis">¿No tienes una cuenta? ¡Crea una <a href='IniciarSesion'>aquí</a>!</p>
                     </div>
-                    
-                    
+                    <div className="imagen">
+                        <img className="img-fluid w-100" src="imagenes/perrito2.png" />
+                    </div>
                 </div>
             </div>
 
