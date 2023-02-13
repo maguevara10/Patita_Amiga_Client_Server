a1c69ab9c61d8dc7c8cc1b883704c08ff1d66ee3
Chaps0218 <105067369+maguevara10@users.noreply.github.com>
2023-02-13 01:24:31 -0500

    Primer Paso hacia el llenado automático
    Se leen los animales, en su totalidad, se posicionan de manera adecuada.
    Falta: Popup con la información detallada

--- client/src/pages/animales.js
+++ client/src/pages/animales.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function Animales() {
+    const [animalList, setAnimalList] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:3001/api/get').then((response) => {
+            setAnimalList(response.data);
+        })
+    }, []);
 
     return (
         <div class="infoPerros gridCentrado">
@@ -27,174 +35,21 @@ function Animales() {
             </div>
 
             <div class="gridCentrado grid4Columnas">
-                {/* <!-- Perro 1 --> */}
-                <div class="smallbox borderbox adulto blanco" id="perro1">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito1.png" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
+            {animalList.map((val) => {
+                    return <div class="smallbox borderbox adulto" id={val.ani_Id}>
+                        <div class="relativo">
+                            <img class="img-fluid w-100" src={val.ani_Foto} />
+                            <div class="overlay">
+                                <p>
+                                    {val.ani_Descripcion}
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <h2 class="font-weight-bold">{val.ani_Nombre}</h2>
                         </div>
                     </div>
-                    <div>
-                        <h2 class="font-weight-bold">Lunita</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 2 -->  */}
-                <div class="smallbox borderbox cachorro" id="perro2">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito2.png" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Pepinillo</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 3 -->  */}
-                <div class="smallbox borderbox" id="perro3">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito3.png" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 3</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 4 -->  */}
-                <div class="smallbox borderbox adulto blanco" id="perro4">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito4.png" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 4</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 5 -->  */}
-                <div class="smallbox borderbox blanco" id="perro5">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito5.png" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 5</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 6 --> */}
-                <div class="smallbox borderbox cachorro blanco" id="perro6">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito6.jpg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 6</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 7 --> */}
-                <div class="smallbox borderbox cuidados_especiales blanco" id="perro7">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito7.jpg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 7</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 8 --> */}
-                <div class="smallbox borderbox adulto" id="perro8">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito8.jpeg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 8</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 9 --> */}
-                <div class="smallbox borderbox blanco" id="perro9">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito9.jpg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 9</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 10 --> */}
-                <div class="smallbox borderbox blanco" id="perro10">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito10.jpeg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 10</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 11 -->  */}
-                <div class="smallbox borderbox adulto" id="perro11">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito11.jpg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 11</h2>
-                    </div>
-                </div>
-                {/* <!-- Perro 12 --> */}
-                <div class="smallbox borderbox blanco" id="perro10">
-                    <div class="relativo">
-                        <img class="img-fluid w-100" src="imagenes/perrito12.jpg" />
-                        <div class="overlay">
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil nam vel repellat harum sapiente consectetur maiores, veniam non, facilis est beatae dicta accusantium similique odio ducimus quaerat, quia soluta amet?
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <h2 class="font-weight-bold">Perrito 12</h2>
-                    </div>
-                </div>
+                })}
             </div>
         </div>
     );
