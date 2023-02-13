3a519e31f17dc4ca8b6e04d5e7dbc0b96bb438fd
MalennyShared <105263362+MalennyShared@users.noreply.github.com>
2023-02-13 02:56:36 -0500

    Agrega pagina mi cuenta

---
+++ client/src/pages/miCuenta.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+function Contactanos() {
+    return (
+
+        <div id='detalles'>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            
+            <fieldset> 
+            <legend><em>Personal</em></legend>
+            <section id='bordeGeneral' class='gridCentrado'>
+                <div class="titulo">
+                    <p>Nombres:</p>
+                </div>
+                <div class="usuario">
+                    <p>Melanny Shared</p>
+                </div>
+                <div class="titulo">
+                    <p>Apellidos:</p>
+                </div>
+                <div class="usuario">
+                    <p>Tinoco Dominguez</p>
+                </div>
+            </section>
+            </fieldset> 
+            <br/>
+            <fieldset> 
+            <legend><em>Contactos</em></legend>
+            <section id='bordeGeneral' class='gridCentrado'>
+                <div class="titulo">
+                    <p>Correo:</p>
+                </div>
+                <div class="usuario">
+                    <p>mstinoco@gmailcom</p>
+                </div>
+                <div class="titulo">
+                    <p>Teléfono:</p>
+                </div>
+                <div class="usuario">
+                    <p>097879638</p>
+                </div>
+            </section>
+            </fieldset> 
+            <br/>
+            <fieldset> 
+            <legend><em>Seguridad</em></legend>
+            <section id='bordeGeneral' class='gridCentrado'>
+                <div class="titulo">
+                    <p>Contraseña:</p>
+                </div>
+                <div class="usuario">
+                    <p>PatitaAmiga</p>
+                </div>
+
+                <br/>
+                <br/>
+            </section>
+            </fieldset> 
+            <br/>
+            <br/>
+            
+        </div>
+
+    );
+}
+export default Contactanos;
