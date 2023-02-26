449c2f70a6dfe89ee4b3a855d36c395668330b0e
DavidGLeal <adguarderas@espe.edu.ec>
2023-02-26 00:35:44 -0500

    Agrego función de email
    Aquí agrego la función para enviar emails con Emailjs. Se deben instalar la librería con npm install @emailjs/browser --save. El correo se envía a la cuenta patita.amiga8011@gmail.com Contraseña: VivaLaFauna2023. Ojalá todo esté correcto.

--- client/src/pages/contactanos.js
+++ client/src/pages/contactanos.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import emailjs from '@emailjs/browser';
+
 
 function Contactanos() {
+
+    let form = document.getElementById("form");
+
+    const enviarEmail = (event) =>{
+        event.preventDefault();
+        emailjs.sendForm('service_c1ykv4a','template_88c3mde',event.target,'EU2y4JZOmGaRFzaUE')
+        .then(response => {
+            form.reset();
+            console.log(response);
+            alert("Correo enviado con éxito.");
+            
+        })
+        .catch(error => console.log(error));
+
+    }
+
     return (
         <div>
             <div class="pres">
@@ -42,24 +60,24 @@ function Contactanos() {
                     <section id="secc">
                         <fieldset id="cuentanos">
                             <legend><h1>Cuéntanos tus dudas</h1></legend>
-                            <form>
+                            <form onSubmit={enviarEmail} id="form" >
                                 <table>
                                     <tr>
                                         <td>
-                                            <input type="name" class="escrib" placeholder="Nombre" />
+                                            <input type="name" class="escrib" placeholder="Nombre" name="Nombre"/>
                                         </td>
                                         <td>
-                                            <input type="email" class="escrib" placeholder="Correo" />
+                                            <input type="email" class="escrib" placeholder="Correo" name="Email"/>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td colspan="2">
-                                            <input type="text" class="asunto" placeholder="Asunto" />
+                                            <input type="text" class="asunto" placeholder="Asunto" name="Asunto"/>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td colspan="2">
-                                            <input type="text" class="mensaje" placeholder="Escribe tu mensaje..." />
+                                            <textarea className="mensaje" placeholder="Escribe tu mensaje..." name = "Mensaje"/>
                                         </td>
                                     </tr>
                                 </table>
